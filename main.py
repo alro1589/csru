b7ee5e069ec44f59f776d005a1759e6c7379ee7b
alro1589 <alro1589@colorado.edu>
2023-04-19 17:32:55 -0600

    rankup & derank

--- main.py
+++ main.py
@@ -19,33 +19,80 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user} has connected to the server: {GUILD}')
 
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
     
-    if message.content.startswith("!rankup"):
-        await rankup(message.author)
+
+@bot.command(pass_context = True)
+async def commands(ctx):
+   await ctx.send(
+      '!commands : Lists all commands\n'
+      '!rankup : Changes your role to the next rank\n'
+      '!derank : Changes your role to the previous rank\n'
+      )
 
 
-async def rankup(message):
-    print("Ranking up...")
-    ranks = set(re.findall("gold nova 1|gold nova 2|gold nova 3|gold nova master|master guardian 1|master guardian 2", message.content, re.IGNORECASE))
+@bot.command(pass_context = True)
+async def rankup(ctx):
+    user = ctx.message.author
+    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian"]
 
-    if ranks:
-        server = bot.get_guild(GUILD)
+    global index
+    index = 0
 
-        roles = [discord.utils.get(server.roles, name=ranks.lower()) for rank in ranks]
+    for i in range(8):
+        if(ranksArr[i] == ranksArr[i] in [y.name for y in user.roles]):
+           index = i
 
-        member = await server.fetch_member(message.author.id)
 
-        try:
-            await member.add_roles(*roles)
-        except Exception as e:
-            print(e)
-            await message.channel.send("Error.")
-        else:
-            await message.channel.send("Ranked up.")
+    
+    currRank = ranksArr[index]
+    currRole = discord.utils.get(user.guild.roles, name=currRank)
+    rankUp = ranksArr[index + 1]
+    rankupRole = discord.utils.get(user.guild.roles, name=rankUp)
+
+    if(rankupRole in user.roles):
+       rankUp = ranksArr[index + 1]
+       rankupRole = discord.utils.get(user.guild.roles, name=rankUp)
+       await user.add_roles(rankupRole)
+       print("added role: " + rankUp)
+       await user.remove_roles(currRole)
+       print("removed role: " + currRank)
+    else:
+       await user.add_roles(rankupRole)
+       await user.remove_roles(currRole)
+    
+    await ctx.send('Ranked up to ' + rankUp)
+
+@bot.command(pass_context = True)
+async def derank(ctx):
+    user = ctx.message.author
+    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian"]
+
+    global index
+    index = 0
+
+    for i in range(8):
+        if(ranksArr[i] == ranksArr[i] in [y.name for y in user.roles]):
+           index = i
+
+
+    
+    currRank = ranksArr[index]
+    currRole = discord.utils.get(user.guild.roles, name=currRank)
+    deRank = ranksArr[index - 1]
+    deRankRole = discord.utils.get(user.guild.roles, name=deRank)
+
+    if(deRankRole in user.roles):
+       deRank = ranksArr[index - 1]
+       deRankRole = discord.utils.get(user.guild.roles, name=deRank)
+       await user.add_roles(deRankRole)
+       print("added role: " + deRank)
+       await user.remove_roles(currRole)
+       print("removed role: " + currRank)
+    else:
+       await user.add_roles(deRankRole)
+       await user.remove_roles(currRole)
+    
+    await ctx.send('Deranked to ' + deRank)
+
 
 bot.run(TOKEN)
