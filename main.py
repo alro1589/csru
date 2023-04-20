d899491aa2b2685db1fd1fc9a352691b951f3c7f
alro1589 <alro1589@colorado.edu>
2023-04-19 19:03:51 -0600

    Fine tuned

--- main.py
+++ main.py
@@ -13,32 +13,34 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 #create client instance
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', activity=discord.Activity(type=discord.ActivityType.watching, name="!h for help"))
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to the server: {GUILD}')
 
-    
-
 @bot.command(pass_context = True)
-async def commands(ctx):
-   await ctx.send(
-      '!commands : Lists all commands\n'
+async def h(ctx):
+    embed = discord.Embed(title='Help', description='Lists all commands compatible', colour=discord.Colour.blue())
+    embed.set_footer(text='!commands : Lists all commands\n'
       '!rankup : Changes your role to the next rank\n'
       '!derank : Changes your role to the previous rank\n'
-      )
+      '!rank : Sets specified users rank to specified rank Usage: !rank @user "rank"\n')
+    await ctx.send(embed=embed)
 
 
 @bot.command(pass_context = True)
+@commands.has_role("CSGO")
 async def rankup(ctx):
+    embed = discord.Embed(title='Rankup', description='', colour=discord.Colour.blue())
+    
     user = ctx.message.author
-    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian"]
+    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian", "Legendary Eagle", "Legendary Eagle Master", "Supreme Master First Class", "Global Elite"]
 
     global index
     index = 0
 
-    for i in range(8):
+    for i in range(12):
         if(ranksArr[i] == ranksArr[i] in [y.name for y in user.roles]):
            index = i
 
@@ -60,22 +62,23 @@ async def rankup(ctx):
        await user.add_roles(rankupRole)
        await user.remove_roles(currRole)
     
-    await ctx.send('Ranked up to ' + rankUp)
+    embed.set_footer(text=f'{user.display_name} ranked up to ' + rankUp)
+    await ctx.send(embed=embed)
 
 @bot.command(pass_context = True)
 async def derank(ctx):
+    embed = discord.Embed(title='Derank', description='', colour=discord.Colour.blue())
+
     user = ctx.message.author
-    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian"]
+    ranksArr = ["Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian", "Legendary Eagle", "Legendary Eagle Master", "Supreme Master First Class", "Global Elite"]
 
     global index
     index = 0
 
-    for i in range(8):
+    for i in range(12):
         if(ranksArr[i] == ranksArr[i] in [y.name for y in user.roles]):
            index = i
 
-
-    
     currRank = ranksArr[index]
     currRole = discord.utils.get(user.guild.roles, name=currRank)
     deRank = ranksArr[index - 1]
@@ -91,8 +94,14 @@ async def derank(ctx):
     else:
        await user.add_roles(deRankRole)
        await user.remove_roles(currRole)
-    
-    await ctx.send('Deranked to ' + deRank)
+
+    embed.set_footer(text=f'{user.display_name} deranked to ' + deRank)
+    await ctx.send(embed=embed)
+
+@bot.command(pass_context=True)
+async def rank(ctx, user : discord.Member, role : discord.Role):
+   await user.add_roles(role)
+   await ctx.send(f"Set {user.display_name}'s rank to {role.name}")
 
 
 bot.run(TOKEN)
